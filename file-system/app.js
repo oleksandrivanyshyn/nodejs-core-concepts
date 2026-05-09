9b8fd8d1c3431f70f793e0c9a487421cb26f9927
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-05-09 14:10:22 +0300

    feat: enhance file watcher to monitor command.txt and read changes dynamically

--- file-system/app.js
+++ file-system/app.js
@@ -1,8 +1,19 @@
 const fs = require('fs/promises');
 
 (async () => {
-    const watcher = await fs.watch('./');
+    const commandFileHandler = await fs.open('./command.txt', 'r');
+    const watcher = await fs.watch('./command.txt');
     for await (const event of watcher) {
-        console.log(event);
+        if(event.eventType === 'change'){
+            const {size} = await commandFileHandler.stat();
+
+            const buffer = Buffer.alloc(size);
+            const offset = 0;
+            const length = buffer.length;
+            const position = 0;
+
+            const content = await commandFileHandler.read({buffer, offset, length, position});
+            console.log(content.buffer.toString('utf-8'));
+        }
     }
 })()
