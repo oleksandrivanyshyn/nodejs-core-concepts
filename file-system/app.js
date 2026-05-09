638ace16d6129c17e81414fc8186edaf6a1ae982
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-05-09 15:08:50 +0300

    feat: add file creation logic based on command.txt input

--- file-system/app.js
+++ file-system/app.js
@@ -1,6 +1,24 @@
 const fs = require('fs/promises');
 
 (async () => {
+    const createFile = async (filePath) => {
+        try{
+            const fileHandler = await fs.open(filePath, 'r');
+            fileHandler.close();
+            return console.log(`File ${filePath} already exists`);
+        }
+        catch(err){
+                const fileHandler = await fs.open(filePath, 'w');
+                console.log(`File ${filePath} created`);
+                fileHandler.close();
+
+        }
+
+
+    }
+    //commands
+    const CREATE_FILE_COMMAND = 'create a file';
+
     const commandFileHandler = await fs.open('./command.txt', 'r');
     
     commandFileHandler.on('change', async () => {
@@ -12,7 +30,11 @@ const fs = require('fs/promises');
         const position = 0;
 
         const content = await commandFileHandler.read({buffer, offset, length, position});
-        console.log(content.buffer.toString('utf-8'));
+        const command = content.buffer.toString('utf-8');
+        if(command.includes(CREATE_FILE_COMMAND)){
+            const filePath = command.substring(CREATE_FILE_COMMAND.length + 1);
+            await createFile(filePath);
+        }
     });
 
     // watcher...
