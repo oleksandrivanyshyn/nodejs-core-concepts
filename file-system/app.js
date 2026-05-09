b2fbeaee5d0b5d3df8f5616aaec86a3a0d07f471
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-05-10 01:29:16 +0300

    feat: add templates for file operations with delete, rename

--- file-system/app.js
+++ file-system/app.js
@@ -1,23 +1,45 @@
 const fs = require('fs/promises');
 
 (async () => {
+
+
     const createFile = async (filePath) => {
         try{
             const fileHandler = await fs.open(filePath, 'r');
-            fileHandler.close();
+            await  fileHandler.close();
             return console.log(`File ${filePath} already exists`);
         }
         catch(err){
                 const fileHandler = await fs.open(filePath, 'w');
                 console.log(`File ${filePath} created`);
-                fileHandler.close();
+                await fileHandler.close();
 
         }
 
 
     }
+    const deleteFile = async (filePath) => {
+        try{
+            await fs.unlink(filePath);
+            console.log(`File ${filePath} deleted`);
+        }
+        catch(err){
+            console.log(`File ${filePath} does not exist`);
+        }
+
+    }
+    const renameFile = async (filePath, newFilePath) => {
+
+
+    }
+    const addToFile = async (filePath, content) => {
+
+    }
     //commands
     const CREATE_FILE_COMMAND = 'create a file';
+    const DELETE_FILE_COMMAND = 'delete a file';
+    const RENAME_FILE_COMMAND = 'rename a file';
+    const ADD_TO_FILE_COMMAND = 'add to a file';
 
     const commandFileHandler = await fs.open('./command.txt', 'r');
     
@@ -31,9 +53,32 @@ const fs = require('fs/promises');
 
         const content = await commandFileHandler.read({buffer, offset, length, position});
         const command = content.buffer.toString('utf-8');
+
+        //create a file
+        //create a file <path>
         if(command.includes(CREATE_FILE_COMMAND)){
             const filePath = command.substring(CREATE_FILE_COMMAND.length + 1);
             await createFile(filePath);
+        }
+        //delete a file
+        //delete a file <path>
+        if(command.includes(DELETE_FILE_COMMAND)){
+            const filePath = command.substring(DELETE_FILE_COMMAND.length + 1);
+        }
+        //rename a file
+        //rename a file <path> <new-path>
+        if(command.includes(RENAME_FILE_COMMAND)){
+            const _idx = command.indexOf(' to ');
+            const filePath = command.substring(RENAME_FILE_COMMAND.length + 1, _idx);
+            const newFilePath = command.substring(_idx + 4);
+            await renameFile(filePath, newFilePath);
+        }
+        //add to a file
+        //add to a file <path> this <content> content: <content>
+        if(command.includes(ADD_TO_FILE_COMMAND)){
+            const _idx = command.indexOf(' this content: ');
+            const filePath = command.substring(ADD_TO_FILE_COMMAND.length + 1, _idx);
+
         }
     });
 
