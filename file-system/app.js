5c366fc01e754aefe199f5236cef5d2585f968c5
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-05-09 14:13:31 +0300

    feat: refactor command.txt watcher to use event-based change handling

--- file-system/app.js
+++ file-system/app.js
@@ -2,18 +2,24 @@ const fs = require('fs/promises');
 
 (async () => {
     const commandFileHandler = await fs.open('./command.txt', 'r');
+    
+    commandFileHandler.on('change', async () => {
+        const {size} = await commandFileHandler.stat();
+
+        const buffer = Buffer.alloc(size);
+        const offset = 0;
+        const length = buffer.length;
+        const position = 0;
+
+        const content = await commandFileHandler.read({buffer, offset, length, position});
+        console.log(content.buffer.toString('utf-8'));
+    });
+
+    // watcher...
     const watcher = await fs.watch('./command.txt');
     for await (const event of watcher) {
         if(event.eventType === 'change'){
-            const {size} = await commandFileHandler.stat();
-
-            const buffer = Buffer.alloc(size);
-            const offset = 0;
-            const length = buffer.length;
-            const position = 0;
-
-            const content = await commandFileHandler.read({buffer, offset, length, position});
-            console.log(content.buffer.toString('utf-8'));
+            commandFileHandler.emit('change');
         }
     }
 })()
