093fedd533f66f99822994623c31c196ce0071cf
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-05-10 12:43:46 +0300

    feat: reduce write count in writeMany and improve backpressure handling in readBig

--- streams/read-big/readBig.js
+++ streams/read-big/readBig.js
@@ -2,12 +2,15 @@ const fs = require('node:fs/promises');
 
 (async () => {
     const fileHandleRead = await fs.open('src.txt', 'r');
-    const fileHandleWrite = await fs.open('src.txt', 'w');
+    const fileHandleWrite = await fs.open('dist.txt', 'w');
 
     const streamRead = fileHandleRead.createReadStream();
     const streamWrite = fileHandleWrite.createWriteStream();
 
     streamRead.on('data', (chunk) => {
-        streamWrite.write(chunk);
+        if(!streamWrite.write(chunk)) streamRead.pause();
     });
+    streamWrite.on('drain', () => {
+        streamRead.resume();
+    })
 })()
