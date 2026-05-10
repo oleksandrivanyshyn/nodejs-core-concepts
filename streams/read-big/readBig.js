04643fa3db0fed69f85dcd37cfaabaf2c88cef4b
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-05-10 13:06:11 +0300

    feat: improve readBig handling by managing split numbers and fixing chunk merging logic

--- streams/read-big/readBig.js
+++ streams/read-big/readBig.js
@@ -7,7 +7,19 @@ const fs = require('node:fs/promises');
     const streamRead = fileHandleRead.createReadStream();
     const streamWrite = fileHandleWrite.createWriteStream();
 
+    let split = '';
     streamRead.on('data', (chunk) => {
+        const numbers = chunk.toString().split('  ');
+        if(Number(numbers[0] + 1) !== Number(numbers[1])){
+            if(split){
+                numbers[0] = split.trim() + numbers[0].trim();
+            }
+        }
+        if(Number(numbers[numbers.length - 2] + 1) !== Number(numbers[numbers.length-1])){
+            split = numbers.pop();
+        }
+        
+
         if(!streamWrite.write(chunk)) streamRead.pause();
     });
     streamWrite.on('drain', () => {
