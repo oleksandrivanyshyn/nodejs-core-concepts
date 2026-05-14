6aa16ee02e15f68db7da99493b03bab1daadb175
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-05-14 17:35:26 +0300

    feat: enhance chat app with message broadcasting and user input handling

--- networking/chat-app/server.js
+++ networking/chat-app/server.js
@@ -1,11 +1,21 @@
-const net = require("net");
+const net = require('net');
 
 const server = net.createServer();
 
-server.on("connection", (socket) => {
-  console.log("A new connection to the server!");
+const clients = [];
+
+server.on('connection', (socket) => {
+  console.log('A new connection to the server!');
+
+  socket.on('data', (data) => {
+    for (const client of clients) {
+      console.log(client.address());
+      client.write(data);
+    }
+  });
+  clients.push(socket);
 });
 
-server.listen(3008, "127.0.0.1", () => {
-  console.log("opened server on", server.address());
+server.listen(3008, '127.0.0.1', () => {
+  console.log('opened server on', server.address());
 });
