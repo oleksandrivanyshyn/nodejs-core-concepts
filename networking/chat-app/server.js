d6b670b1610dbcd26e00b75fa75328e3f32a9b9d
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-05-15 12:35:11 +0300

    feat: add user join/leave notifications and improve message broadcasting in chat app

--- networking/chat-app/server.js
+++ networking/chat-app/server.js
@@ -1,29 +1,39 @@
-const net = require("net");
+const net = require('net');
 
 const server = net.createServer();
 
 // an array of client sockets
 const clients = [];
 
-server.on("connection", (socket) => {
-  console.log("A new connection to the server!");
+server.on('connection', (socket) => {
+  console.log('A new connection to the server!');
 
   const clientId = clients.length + 1;
   socket.write(`id-${clientId}`);
 
-  socket.on("data", (data) => {
-    const dataString = data.toString("utf-8");
-    const id = dataString.substring(0, dataString.indexOf("-"));
-    const message = dataString.substring(dataString.indexOf("-message-") + 9);
+  clients.map((client) => {
+    client.socket.write(`User ${clientId} joined`);
+  });
+
+  socket.on('data', (data) => {
+    const dataString = data.toString('utf-8');
+    const id = dataString.substring(0, dataString.indexOf('-'));
+    const message = dataString.substring(dataString.indexOf('-message-') + 9);
 
     clients.map((client) => {
       client.socket.write(`> User ${id}: ${message}`);
     });
   });
 
+  socket.on('end', () => {
+    clients.map((client) => {
+      client.socket.write(`User ${clientId} disconnected!`);
+    });
+  });
+
   clients.push({ id: clientId.toString(), socket });
 });
 
-server.listen(3008, "127.0.0.1", () => {
-  console.log("opened server on", server.address());
+server.listen(3008, '127.0.0.1', () => {
+  console.log('opened server on', server.address());
 });
