46f5b712750e655282b3594f56beba12fdcb960b
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-05-18 14:10:14 +0300

    feat: enhance custom HTTP library with routing, JSON responses, and static file serving

--- http/simple-web/butter.js
+++ http/simple-web/butter.js
@@ -1,31 +1,64 @@
-const http = require('http');
-const fs = require('fs/promises');
+const http = require("node:http");
+const fs = require("node:fs/promises");
 
 class Butter {
   constructor() {
     this.server = http.createServer();
+    /**
+     * {
+     *  "get/": () => { ... },
+     *  "post/upload": () => { ... }
+     * }
+     *
+     *
+     * this.routes["get/"]()
+     *
+     */
     this.routes = {};
-    this.server.on('request', (req, res) => {
-      //Send a file back to the client
+
+    this.server.on("request", (req, res) => {
+      // Send a file back to the client
       res.sendFile = async (path, mime) => {
-        const fileHandle = await fs.open(path, 'r');
+        const fileHandle = await fs.open(path, "r");
         const fileStream = fileHandle.createReadStream();
-        res.setHeader('Content-Type', mime);
+
+        res.setHeader("Content-Type", mime);
+
         fileStream.pipe(res);
       };
+
+      // Set the status code of the response
+      res.status = (code) => {
+        res.statusCode = code;
+        return res;
+      };
+
+      // Send a json data back to the client (for small json data, less than the highWaterMark)
+      res.json = (data) => {
+        res.setHeader("Content-Type", "application/json");
+        res.end(JSON.stringify(data));
+      };
+
+      // If the routes object does not have a key of req.method + req.url, return 404
+      if (!this.routes[req.method.toLocaleLowerCase() + req.url]) {
+        return res
+          .status(404)
+          .json({ error: `Cannot ${req.method} ${req.url}` });
+      }
 
       this.routes[req.method.toLowerCase() + req.url](req, res);
     });
   }
 
-  listen = (port, cb) => {
+  route(method, path, cb) {
+    this.routes[method + path] = cb;
+  }
+
+  listen(port, cb) {
     this.server.listen(port, () => {
       cb();
     });
-  };
-  route = (method, route, cb) => {
-    this.routes[method + route] = cb;
-  };
+  }
 }
 
 module.exports = Butter;
--- http/simple-web/server.js
+++ http/simple-web/server.js
@@ -1,11 +1,25 @@
-const Butter = require('./butter');
+const Butter = require("./butter");
 
 const PORT = 4060;
 
 const server = new Butter();
 
-server.route('get', '/', (req, res) => {
-  res.sendFile('./public/index.html', 'text/html');
+server.route("get", "/", (req, res) => {
+  res.sendFile("./public/index.html", "text/html");
 });
 
-server.listen(PORT, () => {});
+server.route("get", "/styles.css", (req, res) => {
+  res.sendFile("./public/styles.css", "text/css");
+});
+
+server.route("get", "/scripts.js", (req, res) => {
+  res.sendFile("./public/scripts.js", "text/javascript");
+});
+
+server.route("post", "/login", (req, res) => {
+  res.status(400).json({ message: "Bad login info." });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server has started on port ${PORT}`);
+});
